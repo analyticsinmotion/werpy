312f7eb61049432ad1756f24af21b4adb0bfc07e
Ross Armstrong <52817125+rossarmstrong@users.noreply.github.com>
2023-04-27 16:39:32 +1000

    Initial commit

---
+++ werpy/__init__.py
@@ -0,0 +1,7 @@
+from .normalize import normalize
+from .metrics import metrics
+from .wer import wer
+from .wers import wers
+from .werp import werp
+from .werps import werps
+from .summary import summary
