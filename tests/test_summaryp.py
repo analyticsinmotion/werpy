6e309256d14b97fc815b9af9f277ee6e761ebac5
Ross Armstrong <52817125+rossarmstrong@users.noreply.github.com>
2023-11-17 13:55:51 +1100

    Added unit test for the summaryp module

---
+++ tests/test_summaryp.py
@@ -0,0 +1,81 @@
+"""
+test_summaryp.py
+
+This module contains a set of unit tests for the 'summaryp' function in the 'werpy' package.
+
+The 'summaryp' function offers the capability to incorporate penalty weights for individual 
+errors and displays the weighted Word Error Rate (WER) at the conclusion of its analysis. 
+This function computes the Word Error Rate (WER) and alignment on a per-sequence basis, 
+rather than aggregating across a corpus. It takes reference and hypothesis text sequences 
+as input, represented as lists of words. The output is a dataframe that includes WER, the 
+count of insertions, deletions, and substitutions, along with a detailed breakdown of specific 
+word errors (insertions, deletions, substitutions) necessary to align the hypothesis with the 
+reference for each sequence pair. This enhancement ensures transparent per-sequence WER analysis 
+and facilitates error inspection, going beyond the conventional corpus-level WER assessment.
+
+To run the tests, execute this module as the main program.
+
+For more details on the 'summaryp' function and how to use it, please refer to the 'werpy' package documentation.
+
+Note: If the 'summaryp' module is not imported successfully, an ImportError is raised 
+to ensure that the required module is available for testing.
+"""
+
+import unittest
+import pandas as pd
+
+try:
+    from werpy.summaryp import summaryp
+except ImportError as exc:
+    raise ImportError("The 'summaryp' module could not be imported. Make sure it is installed.") from exc
+
+class TestSummaryp(unittest.TestCase):
+    """
+    This class contains unit tests for the 'summaryp' function, which calculates the Word Error Rate (WER) 
+    between reference and hypothesis text sequences, displaying the summary results as a dataframe.
+    """
+    def test_summaryp_example_1(self):
+        """
+        Test the summaryp function with multiple reference and hypothesis strings.
+
+        This test evaluates the SUMMARYP function with multiple reference and hypothesis text sequences. 
+        It verifies that the calculated SUMMARYP dataframe aligns with the expected output dataframe.
+
+        """
+        ref = ['the tower caused minor discontent because it blocked sight lines of central park',
+               'her father was an alderman in the city government', 
+               'he was commonly referred to as the blacksmith of ballinalee']
+        hyp = ['the tower caused minor discontent because it blocked sightlines of central park',
+               'our father was an alderman in the city government', 
+               'he was commonly referred to as the blacksmith of balen alley']
+
+        # Generate the actual_result DataFrame
+        actual_result = summaryp(ref, hyp)
+
+        data = {
+            'wer': [0.153846, 0.111111, 0.200000],
+            'werp': [0.115385, 0.111111, 0.150000],
+            'ld': [2, 1, 2],
+            'm': [13, 9, 10],
+            'insertions': [0, 0, 1],
+            'deletions': [1, 0, 0],
+            'substitutions': [1, 1, 1],
+            'inserted_words': [[], [], ['balen']],
+            'deleted_words': [['sight'], [], []],
+            'substituted_words': [[('lines', 'sightlines')], [('her', 'our')], [('ballinalee', 'alley')]]
+            }
+
+        expected_result = pd.DataFrame(data)
+
+        # Set the data type of the "ld" column to int64
+        expected_result['ld'] = expected_result['ld'].astype('int32')
+
+        try:
+            pd.testing.assert_frame_equal(expected_result, actual_result)
+            print("DataFrames are equal.")
+        except AssertionError as e:
+            print("DataFrames are not equal. Differences:\n", e)
+
+
+if __name__ == '__main__':
+    unittest.main()
