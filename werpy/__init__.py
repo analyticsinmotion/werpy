797c47df656ef00ba0a89ddcdfb34288b4f27506
Ross Armstrong <52817125+rossarmstrong@users.noreply.github.com>
2023-11-23 14:37:56 +1100

    update metrics import

--- werpy/__init__.py
+++ werpy/__init__.py
@@ -4,7 +4,7 @@ The werpy package provides tools for calculating word error rates (WERs) and rel
 
 from .errorhandler import error_handler
 from .normalize import normalize
-from .metrics import metrics
+from . import metrics
 from .wer import wer
 from .wers import wers
 from .werp import werp
