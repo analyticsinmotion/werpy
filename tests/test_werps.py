d70bd96a458467c8fca888c33c7b42124c3343cb
Ross Armstrong <52817125+rossarmstrong@users.noreply.github.com>
2023-11-29 13:35:17 +1100

    Update unittest for the werps module

--- tests/test_werps.py
+++ tests/test_werps.py
@@ -23,121 +23,140 @@ to ensure that the required module is available for testing.
 import unittest
 from werpy.werps import werps
 
+
 class TestWerps(unittest.TestCase):
     """
-    This class contains unit tests for the 'werps' function, which calculates the weighted Word Error Rate (WER) 
+    This class contains unit tests for the 'werps' function, which calculates the weighted Word Error Rate (WER)
     between reference and hypothesis text sequences.
     """
+
     def test_werps_example_1(self):
         """
         Test the werps function with a simple example.
 
-        This test checks the WERPS function with a basic example of reference and hypothesis strings 
+        This test checks the WERPS function with a basic example of reference and hypothesis strings
         and ensures that the calculated WERPS matches the expected result.
         """
-        self.assertEqual(werps('i love cold pizza', 'i love pizza', 0.5, 0.5, 1), 0.125)
-
+        self.assertEqual(werps("i love cold pizza", "i love pizza", 0.5, 0.5, 1), 0.125)
 
     def test_werps_example_2(self):
         """
         Test the werps function with multiple reference and hypothesis strings.
 
-        This test evaluates the WERPS function with multiple reference and hypothesis text sequences. 
+        This test evaluates the WERPS function with multiple reference and hypothesis text sequences.
         It verifies that the calculated WERPS aligns with the expected results.
 
         """
-        ref = ['i love cold pizza','the sugar bear character was popular']
-        hyp = ['i love pizza','the sugar bare character was popular']
+        ref = ["i love cold pizza", "the sugar bear character was popular"]
+        hyp = ["i love pizza", "the sugar bare character was popular"]
         expected_result = [0.125, 0.16666666666666666]
 
-        self.assertEqual(werps(
-            ref, hyp, insertions_weight=0.5, deletions_weight=0.5, substitutions_weight=1
-            ), expected_result)
-
+        self.assertEqual(
+            werps(
+                ref,
+                hyp,
+                insertions_weight=0.5,
+                deletions_weight=0.5,
+                substitutions_weight=1,
+            ),
+            expected_result,
+        )
 
     def test_werps_example_3(self):
         """
         Test the werps function with multiple reference and hypothesis strings.
 
-        This test evaluates the WERPS function with multiple reference and hypothesis text sequences. 
+        This test evaluates the WERPS function with multiple reference and hypothesis text sequences.
         It verifies that the calculated WERPS aligns with the expected results.
 
         """
-        ref = ['no one else could claim that','she cited multiple reasons why']
-        hyp = ['no one else could claim that','she sighted multiple reasons why']
+        ref = ["no one else could claim that", "she cited multiple reasons why"]
+        hyp = ["no one else could claim that", "she sighted multiple reasons why"]
         expected_result = [0.0, 0.2]
 
         self.assertEqual(werps(ref, hyp, 0.5, 0.5, 1), expected_result)
-
 
     def test_werps_example_4(self):
         """
         Test the werps function with multiple reference and hypothesis strings.
 
-        This test evaluates the WERPS function with multiple reference and hypothesis text sequences. 
+        This test evaluates the WERPS function with multiple reference and hypothesis text sequences.
         It verifies that the calculated WERPS aligns with the expected results.
 
         """
-        ref = ['it was beautiful and sunny today']
-        hyp = ['it was a beautiful and sunny day']
+        ref = ["it was beautiful and sunny today"]
+        hyp = ["it was a beautiful and sunny day"]
         expected_result = [0.25]
 
         self.assertEqual(werps(ref, hyp, 0.5, 0.5, 1), expected_result)
-
 
     def test_werps_example_5(self):
         """
         Test the werps function with multiple reference and hypothesis strings.
 
-        This test evaluates the WERPS function with multiple reference and hypothesis text sequences. 
+        This test evaluates the WERPS function with multiple reference and hypothesis text sequences.
         It verifies that the calculated WERPS aligns with the expected results.
 
         """
-        ref = ['it blocked sight lines of central park', 'her father was an alderman in the city government']
-        hyp = ['it blocked sightlines of central park', 'our father was an elder man in the city government']
+        ref = [
+            "it blocked sight lines of central park",
+            "her father was an alderman in the city government",
+        ]
+        hyp = [
+            "it blocked sightlines of central park",
+            "our father was an elder man in the city government",
+        ]
         expected_result = [0.21428571428571427, 0.2777777777777778]
 
         self.assertEqual(werps(ref, hyp, 0.5, 0.5, 1), expected_result)
-
-
 
     def test_werps_example_6(self):
         """
         Test the werps function with longer text sequences.
 
-        This test assesses the WERPS function's performance with longer reference and hypothesis text sequences. 
+        This test assesses the WERPS function's performance with longer reference and hypothesis text sequences.
         It verifies that the calculated WERPS closely matches the expected results.
 
         """
         ref = [
-            'it is consumed domestically and exported to other countries',
-            'the sugar bear character was popular enough to have occasional premium toys',
-            'it is one of the most watched television networks in the country',
-            'it could be carried and prepared by the individual soldier',
-            'he was executed in a lubyanka prison cellar',
-            'rufino street in makati right inside the makati central business district',
-            'its estuary is considered to have abnormally low rates of dissolved oxygen',
-            'he later cited his first wife anita as the inspiration for the song',
-            'gadya is the nearest rural locality',
-            'taxes are a tool in the adjustment of the economy'
-            ]
+            "it is consumed domestically and exported to other countries",
+            "the sugar bear character was popular enough to have occasional premium toys",
+            "it is one of the most watched television networks in the country",
+            "it could be carried and prepared by the individual soldier",
+            "he was executed in a lubyanka prison cellar",
+            "rufino street in makati right inside the makati central business district",
+            "its estuary is considered to have abnormally low rates of dissolved oxygen",
+            "he later cited his first wife anita as the inspiration for the song",
+            "gadya is the nearest rural locality",
+            "taxes are a tool in the adjustment of the economy",
+        ]
         hyp = [
-            'it is consumed domestically and exported to other countries',
-            'the sugar bare character was popular enough to have occasional premium toys',
-            'it is one of the most watched television networks in the country',
-            'it could be carried and prepared by the individual soldier',
-            'he was executed in alabianca prison seller',
-            'rofino street in mccauti right inside the macasi central business district',
-            'its estiary is considered to have a normally low rates of dissolved oxygen',
-            'he later sighted his first wife anita as the inspiration for the song',
-            'gadia is the nearest rural locality',
-            'taxes are a tool in the adjustment of the economy'
-            ]
-        expected_result = [0.0, 0.08333333333333333, 0.0, 0.0, 0.3125, 0.2727272727272727,
-                           0.20833333333333334, 0.07692307692307693, 0.16666666666666666, 0.0]
+            "it is consumed domestically and exported to other countries",
+            "the sugar bare character was popular enough to have occasional premium toys",
+            "it is one of the most watched television networks in the country",
+            "it could be carried and prepared by the individual soldier",
+            "he was executed in alabianca prison seller",
+            "rofino street in mccauti right inside the macasi central business district",
+            "its estiary is considered to have a normally low rates of dissolved oxygen",
+            "he later sighted his first wife anita as the inspiration for the song",
+            "gadia is the nearest rural locality",
+            "taxes are a tool in the adjustment of the economy",
+        ]
+        expected_result = [
+            0.0,
+            0.08333333333333333,
+            0.0,
+            0.0,
+            0.3125,
+            0.2727272727272727,
+            0.20833333333333334,
+            0.07692307692307693,
+            0.16666666666666666,
+            0.0,
+        ]
 
         self.assertEqual(werps(ref, hyp, 0.5, 0.5, 1), expected_result)
 
 
-if __name__ == '__main__': # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     unittest.main()
