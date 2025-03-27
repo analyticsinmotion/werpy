ffd2de12df20e36e7cf5c7c871e47ecfe38333d9
rossarmstrong <52817125+rossarmstrong@users.noreply.github.com>
2025-03-27 17:11:50 +1100

    Fix naming and built-in override warnings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "werpy"
-copyright = "Copyright 2023-2025, Analytics in Motion"
+copyright = "2023-2025, Analytics in Motion"
 author = "Ross Armstrong"
 release = "3.0.1"
 
