f877a8d08c63aebb4095ff4d5906477d1a32e5c2
rossarmstrong <52817125+rossarmstrong@users.noreply.github.com>
2025-03-28 14:46:03 +1100

    Apply SPDX-compliant license headers to source files

--- tests/test_werp.py
+++ tests/test_werp.py
@@ -1,5 +1,4 @@
 # SPDX-FileCopyrightText: 2023-2025 Analytics in Motion <https://www.analyticsinmotion.com>
-# SPDX-FileType: SOURCE
 # SPDX-License-Identifier: BSD-3-Clause
 
 """
--- werpy/errorhandler.py
+++ werpy/errorhandler.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2023-2025 Analytics in Motion <https://www.analyticsinmotion.com>
+# SPDX-License-Identifier: BSD-3-Clause
+
 """
 This module will handle the exceptions for this package
 """
