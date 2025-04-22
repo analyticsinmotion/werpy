11cbc5ab025cc994ca2a39ad6155cd16efd7890b
rossarmstrong <52817125+rossarmstrong@users.noreply.github.com>
2025-04-22 22:46:42 +1000

    🧪 Testing v3.1.1.dev1

--- werpy/metrics.pyx
+++ werpy/metrics.pyx
@@ -65,7 +65,27 @@ cpdef cnp.ndarray calculations(object reference, object hypothesis):
 
     insertions, deletions, substitutions = 0, 0, 0
     inserted_words, deleted_words, substituted_words = [], [], []
+    i, j = m, n
+    while i > 0 or j > 0:
+        if i > 0 and j > 0 and reference_word[i - 1] == hypothesis_word[j - 1]:
+            i -= 1
+            j -= 1
+        else:
+            if i > 0 and j > 0 and ldm[i, j] == ldm[i - 1, j - 1] + 1:
+                substitutions += 1
+                substituted_words.append((reference_word[i - 1], hypothesis_word[j - 1]))
+                i -= 1
+                j -= 1
+            elif j > 0 and ldm[i, j] == ldm[i, j - 1] + 1:
+                insertions += 1
+                inserted_words.append(hypothesis_word[j - 1])
+                j -= 1
+            elif i > 0 and ldm[i, j] == ldm[i - 1, j] + 1:
+                deletions += 1
+                deleted_words.append(reference_word[i - 1])
+                i -= 1
 
+    inserted_words.reverse(), deleted_words.reverse(), substituted_words.reverse()
 
     return np.array(
         [wer, ld, m, insertions, deletions, substitutions, inserted_words, deleted_words, substituted_words],
